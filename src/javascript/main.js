aaba1844d8e9419efcd9e0abf25da07a59d6ee3b
Sávio Haistém <savio.dantas.5858@gmail.com>
2022-10-25 11:07:51 -0300

    estilizando

--- src/javascript/main.js
+++ src/javascript/main.js
@@ -1,3 +1,26 @@
-function ChangePage(para) { 
-    window.location.pathname = `./${para}`
-};
+class NavBar {
+    constructor(Menu, navlist, navlink) {
+        this.Menu = document.querySelector(Menu);
+        this.navList = document.querySelector(navlist);
+        this.navLinks = document.querySelectorAll(navlink)
+        this.activeClass = 'active'
+    }
+
+    addClickEvent() {
+        this.Menu.addEventListener("click", () => console.log('foi'))
+    }
+
+    init() {
+        if( this.Menu ) {
+            this.addClickEvent();
+        }
+    }
+}
+
+const navbar = new NavBar(
+    '#navbar',
+    '#navlist',
+    '.navlink',
+);
+
+navbar.init();
